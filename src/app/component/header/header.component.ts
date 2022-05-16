78f9f0a4df8d19a9d8b8fc1a921edca8f6b26a52
margaritasing <libiamfreitesm@gmail.com>
2022-05-16 15:44:05 -0300

    agrego componentes

---
+++ src/app/component/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  public totalItem : number = 0;
+  public searchTerm !: string;
+  constructor(private cartService : CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.getProducts()
+    .subscribe(res=>{
+      this.totalItem = res.length;
+    })
+  }
+  search(event:any){
+    this.searchTerm = (event.target as HTMLInputElement).value;  
+    this.cartService.search.next(this.searchTerm);
+  }
+}
